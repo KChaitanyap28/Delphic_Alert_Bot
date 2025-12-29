b64f1076480eb0626cef7b6434e49537ab0d2418
KChaitanya <kchaitanyap28@gmail.com>
2025-12-29 18:41:39 +0530

    Security Fix: Removed hardcoded secrets

--- config/common.py
+++ config/common.py
@@ -2,20 +2,16 @@ import os
 import pytz
 
 class CommonConfig:
-    TG_TOKEN = os.environ.get("7637881639:AAH3BpXgQHdd0Du6nCT-fg3oE7SYbuPaLVA")
-    TG_CHAT_ID = os.environ.get("5440231825")
+    TG_TOKEN = os.environ.get("TG_TOKEN")
+    TG_CHAT_ID = os.environ.get("TG_CHAT")
 
     # Yahoo Finance Symbols
     SYMBOLS = ["GBPUSD=X", "AUDUSD=X", "JPY=X", "EURJPY=X"]
     
-    # Timeframe
     TIMEFRAME = "15m"
-    
-    # Indicators
     SMA_H4_LONG = 640
     SMA_H4_SHORT = 288
     
-    # Symbol Mapping
     SYMBOL_MAP = {
         "GBPUSD=X": "GBPUSDm",
         "AUDUSD=X": "AUDUSDm",
@@ -30,7 +26,6 @@ class CommonConfig:
         "EURJPYm": 0.01 
     }
 
-    # --- S-TIER TIME SETTINGS ---
     TIMEZONE = pytz.timezone("Asia/Kolkata")
     START_HOUR = 6
     START_MINUTE = 30
